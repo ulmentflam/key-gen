94da635c997d43eea4ca66c78b7a92971dc0f185
ulmentflam <johnwowen3@gmail.com>
2024-08-08 18:13:32 -0400

    Deleting Brew Formula

--- Formula/k/key-gen.rb
+++
@@ -1,7 +0,0 @@
-class KeyGen < Formula
-   desc "key-gen is a CLI tool to generate keys for various blockchains."
-   homepage "github.com/ulmentflam/key-gen"
-   url ""
-   sha256 ""
-   license "GPLv3"
-end
